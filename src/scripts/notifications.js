b498f0f6fb2ee8583cc85c4f9bbc55da32f27c67
Jessica Marcus <jessica.marcus@gmail.com>
2014-12-28 18:42:18 -0500

    intermediate checkin re: #20

--- src/scripts/notifications.js
+++ src/scripts/notifications.js
@@ -7,26 +7,54 @@ var notify = notify || {};
 notify.modal = document.getElementById('notification');
 notify.button = document.getElementById('notify-ok');
 
-// remove modal
+notify.init = function () {
+    document.getElementById('quit-button').addEventListener('click', notify.quit, false);
+};
+
+// hide modal
 notify.dismiss = function () {
     notify.modal.className = 'hidden';
 };
-notify.calibration = function (callback) {
-    notify.modal.className = 'notify';
-    notify.modal.firstElementChild.innerHTML = notify.msg.calibration;
 
-    var listener = function () {
-        notify.dismiss();
-        callback();
-        notify.button.removeEventListener('click', listener, false);
-    };
-    // enable dismissal button
+// a user's device must be calibrated in order to control a paddle properly
+notify.calibration = function (callback) {
+    // show first set of instructions to user
+    notify.modal.className = 'notify';
+    notify.modal.firstElementChild.innerHTML = notify.msg.calibration1;
+
+    // guide the user through recording these two values:
+    var heading, offset,
+        getHeading = function () {
+            // record heading and then move to the second calibration stage
+            heading = 'something';
+            notify.modal.firstElementChild.innerHTML = notify.msg.calibration2;
+        },
+
+        getOffset = function () {
+            // record offset and then dismiss the notification
+            offset = 'something else';
+            notify.dismiss();
+            notify.button.removeEventListener('click', listener, false);
+        },
+
+        listener = function () {
+            if (!heading) {
+                getHeading();
+            } else {
+                getOffset();
+            }
+            //notify.dismiss();
+            //callback();
+            //notify.button.removeEventListener('click', listener, false);
+        };
+
+    // enable button
     notify.button.className = 'ok';
     notify.button.addEventListener('click', listener, false);
 };
 
 // if a user's device changes orientation, display a message to the user to suggest disabling screen rotation.
-// note: this notification uses its own specific elements, not the general ones
+// note: this notification uses its own specific elements for display to user, not the general ones
 notify.orientation = function () {
     var modal = document.getElementById('notify-orient'),
         button = document.getElementById('notify-ok-orient'),
@@ -50,18 +78,51 @@ notify.queued = function () {
     notify.modal.firstElementChild.innerHTML = notify.msg.queued;
 };
 
+notify.quit = function () {
+    var buttonQuit = document.getElementById('notify-quit'),
+        buttonNoquit = document.getElementById('notify-noquit'),
+        hideButtons = function () {
+            buttonQuit.className = 'hidden';
+            buttonNoquit.className = 'hidden';
+        },
+        noListener = function () {
+            notify.modal.firstElementChild.innerHTML = "Sweet!";
+            hideButtons();
+            setTimeout(notify.dismiss, 1000);
+            buttonNoquit.removeEventListener('click', noListener, false);
+        };
+        yesListener = function () {
+            notify.modal.firstElementChild.innerHTML = notify.msg.goodbye;
+            document.getElementById('quit-button').className = 'hidden';
+            hideButtons();
+            buttonNoquit.removeEventListener('click', yesListener, false);
+            //TODO: call bleepout.controller.actionPlayerQuit();
+    };
+    notify.modal.className = 'notify';
+    notify.modal.firstElementChild.innerHTML = notify.msg.quit;
+    buttonQuit.className = 'ok';
+    buttonNoquit.className = 'ok';
+
+    buttonQuit.addEventListener('click', yesListener, false);
+    buttonNoquit.addEventListener('click', noListener, false);
+};
+
 // this obj stores all message text strings
 notify.msg = {
     queued: "Welcome to Bleepout! You are in the queue; please wait for the next round.",
     orientation: "Bleepout will be even more awesome if you turn off your phone's screen rotation!",
-    calibration: "Please find your paddle on the wall, point your phone at it, and press OK!"
+    calibration1: "Let's begin calibration.",
+    calibration2: "Please find your paddle on the wall, point your phone at it, and press OK!",
+    quit: "Do you really want to quit?",
+    goodbye: "Thanks for Bleeping Out with us!"
 };
 
-//TODO: as written, there could be collisions with multiple messages being triggered for the one #notification div
-//TODO: add  this orientation event listener when user is taken out of queue state
 // we want modal to appear when the user turns their phone
 window.addEventListener('orientationchange', notify.orientation, false);
-notify.queued();
 
-// temp. test event
-//window.addEventListener('orientationchange', notify.queued, false);
+notify.init();
+
+// test events - delete when complete
+//notify.quit();
+//notify.calibration();
+//window.addEventListener('orientationchange', notify.calibration, false);
